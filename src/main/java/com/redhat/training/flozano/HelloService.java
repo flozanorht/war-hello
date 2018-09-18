dd17d3dfd1f9e2d9847c5b066c05170382ce4be4
Fernando Lozano <flozano@redhat.com>
2018-09-17 22:20:16 -0300

    Small change in the output message

--- src/main/java/com/redhat/training/flozano/HelloService.java
+++ src/main/java/com/redhat/training/flozano/HelloService.java
@@ -3,6 +3,6 @@ package com.redhat.training.flozano;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Hello, " + name + "!\n";
+        return "Hello, " + name + " !\n";
     }
 }
